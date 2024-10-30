7aae58c6ef4ba335693e19d49857865f7799bdae
Daniel Tapia Maldonado <dtapia@klap.cl>
2024-10-30 20:00:30 -0300

    first commit

---
+++ src/main/java/com/gear/mh_hunter_arsenal/MhHunterArsenalApplication.java
@@ -0,0 +1,13 @@
+package com.gear.mh_hunter_arsenal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhHunterArsenalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhHunterArsenalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gear/mh_hunter_arsenal/MhHunterArsenalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gear.mh_hunter_arsenal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhHunterArsenalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
